14bb3c10fa1289e22f6e5b6e354461e8d8be96c5
ahmady016 <ahmady09@gmail.com>
2026-05-07 21:07:40 +0300

    reorder the code sections in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,17 +3,10 @@ Console.WriteLine("=====================");
 Console.WriteLine("The First C# Program");
 Console.WriteLine("=====================");
 
-#region Strings and String Interpolation
-// Strings.RemoveTagsFromHtmlString();
-// Strings.GetAllOccurrencesOfSubstring();
-// Strings.GetSubstring();
-// Strings.GenerateRandomPINs(10);
-// Strings.StringNumericFormatting();
-// Strings.NumberFormatting();
-// Strings.PercentageFormatting();
-// Strings.CurrencyFormatting();
-// Strings.StringInterpolation();
-// Strings.StringCompositeFormatting();
+#region Data Types
+// DataTypes.StringToNumberConversions();
+// DataTypes.Integers();
+// DataTypes.Decimals();
 #endregion
 
 #region Numbers and Math Operations
@@ -30,6 +23,28 @@ Console.WriteLine("=====================");
 // Conditions.CheckSubscriptionStatus();
 #endregion
 
+#region Loops
+// Loops.GetSentencesFromParagraphs();
+// Loops.PrintCities();
+// Loops.GetUserRole();
+// Loops.GetWeekDay();
+// Loops.CountryGuessGame();
+// Loops.HeroBattleGame();
+#endregion
+
+#region Strings and String Interpolation
+// Strings.RemoveTagsFromHtmlString();
+// Strings.GetAllOccurrencesOfSubstring();
+// Strings.GetSubstring();
+// Strings.GenerateRandomPINs(10);
+// Strings.StringNumericFormatting();
+// Strings.NumberFormatting();
+// Strings.PercentageFormatting();
+// Strings.CurrencyFormatting();
+// Strings.StringInterpolation();
+// Strings.StringCompositeFormatting();
+#endregion
+
 #region Arrays
 // Arrays.OldArrayInitialization();
 // Arrays.NewArrayInitialization();
@@ -41,21 +56,6 @@ Console.WriteLine("=====================");
 // Arrays.SortAndReverseArray();
 // Arrays.ReverseStringCharacters();
 // Arrays.ReverseStringWords();
-#endregion
-
-#region Loops
-// Loops.GetSentencesFromParagraphs();
-// Loops.PrintCities();
-// Loops.GetUserRole();
-// Loops.GetWeekDay();
-// Loops.CountryGuessGame();
-// Loops.HeroBattleGame();
-#endregion
-
-#region Data Types
-// DataTypes.StringToNumberConversions();
-// DataTypes.Integers();
-// DataTypes.Decimals();
 #endregion
 
 #region Methods
