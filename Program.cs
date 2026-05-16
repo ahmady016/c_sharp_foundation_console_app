99278f8bd481c3cf0b0d5a6f75bcccbe3a04e834
ahmady016 <ahmady09@gmail.com>
2026-05-16 11:37:41 +0300

    collections => build the ListOfNumbers method

--- Program.cs
+++ Program.cs
@@ -83,6 +83,7 @@ Console.WriteLine("=====================");
 #endregion
 
 #region Collections
+Collections.ListOfNumbers();
 // Collections.ShoppingListCalculator();
 // Collections.ListEmployeesAndJobTitles();
 // Collections.AssignEmployeesToDepartments();
